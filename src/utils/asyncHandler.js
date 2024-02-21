20f9baab9e2325186a1316973d7cae9ed0fcb6df
Harsh Sinha <rajharsh4424@gmail.com>
2024-02-22 02:00:26 +0530

    added comment

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,4 @@
+//High order function
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
