7852de65728e3cbd6ddfce1a745ef85bff59b236
Harsh Sinha <rajharsh4424@gmail.com>
2024-02-26 22:36:57 +0530

    add user account management end points

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -263,4 +263,130 @@ const refreshAccessToken = asyncHandler(async ()=> {
     }
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+
+    const { oldPassword, newPassword } = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.newPassword = newPassword
+
+    await User.save({ validateBeforeSave: false}) //before saving pre hook middleware will be called to hash the new password
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password updated successfully"))
+})
+
+const getCurrentUser = asyncHandler(async (req,res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"))
+})
+
+const updateAccountDetails = asyncHandler(async (req,res) => {
+
+    const { fullname, email } = req.body
+
+    if(!(fullname || email)) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullname,
+                email
+            },
+        },
+        {
+            new: true // it will return the updated user details after the update
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
+
+const updateUserAvatar = asyncHandler(async (req,res) => {
+
+    const avatarLocalPath = req.file?.path // here we have are asking for only avatar image for profile picture i.e file.path not files.path unlike register user controller where we were asking for avatar and cover Image
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url) {
+        throw new ApiError(400, "Error while uploading avatar on cloudinary")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true // it will return the updated user details after the update
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, user, "Avatar Image updated successfully"))
+
+})
+
+const updateUserCoverImage = asyncHandler(async (req,res) => {
+
+    const coverImageLocalPath = req.file?.path // here we have are asking for only avatar image for profile picture i.e file.path not files.path unlike register user controller where we were asking for avatar and cover Image
+
+    if(!coverImageLocalPath) {
+        throw new ApiError(400, "Cover Image file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url) {
+        throw new ApiError(400, "Error while uploading cover image on cloudinary")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true // it will return the updated user details after the update
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, user, "Cover Image updated successfully"))
+    
+})
+
+export { registerUser,
+        loginUser,
+        logoutUser, 
+        refreshAccessToken, 
+        changeCurrentPassword, 
+        getCurrentUser,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUserCoverImage
+     };
